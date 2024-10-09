a1d204e8878bd1b71f4ac8a7a2cb6c8621388d95
SadikRa <sadikrahman494@gmail.com>
2024-10-09 09:22:09 +0600

    banner added done

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,22 +1,15 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import img1 from '../../../assets/01.jpg'
-import img2 from '../../../assets/02.jpg'
-import img3 from '../../../assets/03.png'
-import img4 from '../../../assets/04.jpg'
-import img5 from '../../../assets/05.png'
-import img6 from '../../../assets/06.png'
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/pagination";
 
-import './styles.css';
+import "./styles.css";
 
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Pagination } from "swiper/modules";
 
 export default function App() {
   return (
@@ -28,13 +21,15 @@ export default function App() {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide><img src={img1} /></SwiperSlide>
-        <SwiperSlide><img src={img2} /></SwiperSlide>
-        <SwiperSlide><img src={img3} /></SwiperSlide>
-        <SwiperSlide><img src={img4} /></SwiperSlide>
-        <SwiperSlide><img src={img5} /></SwiperSlide>
-        <SwiperSlide><img src={img6} /></SwiperSlide>
-        
+        <SwiperSlide>
+          <img src="https://i.ibb.co.com/F4nLXcW/Banner1.webp" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.ibb.co.com/xLJnqqN/Banner2.webp" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://i.ibb.co.com/jv4KHTr/Banner3.webp" />
+        </SwiperSlide>
       </Swiper>
     </>
   );
