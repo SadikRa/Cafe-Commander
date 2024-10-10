9f5ed7c1145468dcabda6dbcccb4c6b6875b6790
SadikRa <sadikrahman494@gmail.com>
2024-10-10 21:21:23 +0600

    image text added done

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,25 +1,25 @@
-import React, { useRef, useState } from 'react';
+import React from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
-import './Category.css';
+import "./Category.css";
 
 // import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
+import { EffectCoverflow, Pagination } from "swiper/modules";
 
 export default function Category() {
   return (
     <>
       <Swiper
-        effect={'coverflow'}
+        effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
-        slidesPerView={'auto'}
+        slidesPerView={"auto"}
         coverflowEffect={{
           rotate: 50,
           stretch: 0,
@@ -31,32 +31,23 @@ export default function Category() {
         modules={[EffectCoverflow, Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        <SwiperSlide className="relative">
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="Cake" className="w-full h-full object-cover"/>
+          <h4 className="absolute bottom-4 left-4 text-3xl uppercase text-white shadow p-2">
+            Cake Name
+          </h4>
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        <SwiperSlide className="relative">
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="Cake" className="w-full h-full object-cover"/>
+          <h4 className="absolute bottom-4 left-4 text-3xl uppercase text-white shadow p-2">
+            Cake Name
+          </h4>
         </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        <SwiperSlide className="relative">
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="Cake" className="w-full h-full object-cover"/>
+          <h4 className="absolute bottom-4 left-4 text-3xl uppercase text-white shadow p-2">
+            Cake Name
+          </h4>
         </SwiperSlide>
       </Swiper>
     </>
