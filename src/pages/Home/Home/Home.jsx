199ad2fa8aa3db72af6f8a0454f220f539188b95
SadikRa <sadikrahman494@gmail.com>
2024-10-11 21:45:55 +0600

    problem fixed done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,18 +2,16 @@ import Category from "../../Category/Category";
 import Banner from "../Banner/Banner";
 
 const Home = () => {
-
-
   return (
     <div className="">
-      
       <Banner></Banner>
 
-      <h4 className="text-4xl text-amber-500 font-bold p-2 text-center">Online order item available</h4>
-      
-     
-     <Category></Category>
+      {/* category section */}
+      <h4 className="text-4xl mt-10 text-amber-500 font-bold p-2 text-center">
+        Online order item available
+      </h4>
 
+      <Category></Category>
     </div>
   );
 };
---
+++ src/pages/Home/OurMenu/OurMenu.jsx
@@ -0,0 +1,13 @@
+
+
+const OurMenu = () => {
+    return (
+        <div className="mt-24 mb-24">
+            <h1>
+                this is our menu list 
+            </h1>
+        </div>
+    );
+};
+
+export default OurMenu;
